9c15e9ae39f8f54b3b89b4f4f9367749386e5fa8
Jesus <jesuspalominomartinez1234@gmail.com>
2025-08-12 21:33:22 -0500

    first commit

---
+++ src/main/java/com/customer/business/MsCustomerBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.customer.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCustomerBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCustomerBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customer/business/MsCustomerBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomerBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
